2e173cc239889e19a99bb93154397223d9f84d99
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-05-29 16:31:22 -0700

    Added Monolog

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,10 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
+spl_autoload_register(function ($classname) {
+    require ("../classes/" . $classname . ".php");
+});
+
 $config['displayErrorDetails'] = true;
 
 $config['db']['host']   = "localhost";
@@ -12,9 +16,19 @@ $config['db']['pass']   = "fyvzcuc-ghgbevny";
 $config['db']['dbname'] = "slimphp-tutorial";
 
 $app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
+    $this->logger->addInfo("Something interesting happened");
 
     return $response;
 });

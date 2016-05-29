19cd10bf2593a1a1716c5892b3b15f4450d00504
Alex Mercier <pro.alexandre.mercier@gmail.com>
2016-05-29 16:39:19 -0700

    wip

--- src/public/index.php
+++ src/public/index.php
@@ -16,8 +16,8 @@ $config['db']['pass']   = "fyvzcuc-ghgbevny";
 $config['db']['dbname'] = "slimphp-tutorial";
 
 $app = new \Slim\App(["settings" => $config]);
-
 $container = $app->getContainer();
+
 $container['logger'] = function($c) {
     $logger = new \Monolog\Logger('my_logger');
     $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
@@ -25,11 +25,29 @@ $container['logger'] = function($c) {
     return $logger;
 };
 
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
 $app->get('/hello/{name}', function (Request $request, Response $response) {
     $name = $request->getAttribute('name');
     $response->getBody()->write("Hello, $name");
     $this->logger->addInfo("Something interesting happened");
-
     return $response;
 });
+
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo("Ticket list");
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
+
+    $response->getBody()->write(var_export($tickets, true));
+    return $response;
+});
+
 $app->run();
